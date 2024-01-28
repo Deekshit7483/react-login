92227a6abdf08afff70a03d0a5da13a627e489c2
Deekshith H R <58986536+Deekshit7483@users.noreply.github.com>
2024-01-28 14:27:12 +0530

    Add files via upload

---
+++ src/components/Registration/Registration.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react'
+import { Alert } from 'react-bootstrap';
+import Login from '../Login/Login';
+
+
+function Registration() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
+
+    const [flag, setFlag] = useState(false);
+    const [login, setLogin] = useState(true);
+    const [info, setInfo] = useState(true);
+
+
+
+
+    // on form submit...
+    function handleFormSubmit(e) {
+        e.preventDefault();
+
+        if (!name || !email || !password || !phone) {
+            setFlag(true);
+
+        } else {
+            setFlag(false);
+            localStorage.setItem("hardikSubmissionEmail", JSON.stringify(email));
+            localStorage.setItem("hardikSubmissionPassword", JSON.stringify(password));
+            console.log("Saved in Local Storage");
+
+            setLogin(!login)
+
+        }
+
+    }
+
+    // Directly to the login page
+    function handleClick() {
+        setLogin(!login)
+    }
+
+
+
+    return (
+        <>
+            <nav className="navbar navbar-light">
+                
+            </nav>
+            {info ? <div> {login ? <form onSubmit={handleFormSubmit}>
+                <h3>Register</h3>
+
+                <div className="form-group">
+                    <label>Name</label>
+                    <input type="text" className="form-control" placeholder="Enter Full Name" name="name" onChange={(event) => setName(event.target.value)} />
+                </div>
+
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email" className="form-control" placeholder="Enter email" onChange={(event) => setEmail(event.target.value)} />
+                </div>
+
+
+                <div className="form-group">
+                    <label>Phone Number</label>
+                    <input type="Phone" className="form-control" placeholder="Enter contact number" onChange={(event) => setPhone(event.target.value)} />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" onChange={(event) => setPassword(event.target.value)} />
+                </div>
+
+
+                <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
+                <p className="forgot-password text-right">
+                    Already registered <a href="#" onClick={handleClick} >log in?</a>
+                </p>
+                {flag &&
+                    <Alert color='primary' variant="danger" >
+                        All fields are important
+                </Alert>
+                }
+
+            </form> : <Login />}
+            </div> : 
+            
+            {/*<div>
+                    
+                </div>*/}
+                }
+        </>
+    )
+}
+
+export default Registration
